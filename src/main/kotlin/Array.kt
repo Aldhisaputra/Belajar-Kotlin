1fdc736159a57b23be92742eb5e569c2a29f648a
aldhi saputra <aldhisaputra795@gmail.com>
2024-03-30 22:13:40 +0700

    penjelasan array

--- src/main/kotlin/Array.kt
+++ src/main/kotlin/Array.kt
@@ -1,4 +1,8 @@
-//Kumpulan data yang jenisnya sama
+/*menyimpan beberapa objek di dalam sebuah variabel.Array di Kotlin direpresentasikan
+oleh kelas Array yang memiliki fungsi get dan set serta properti size.
+Untuk membuat sebuah Array kita bisa memanfaatkan sebuah library function arrayOf()
+
+ */
 fun main(){
     val name: Array<String> = arrayOf("adi","ali","abi","ari")
     name.set(1,"ubi")
