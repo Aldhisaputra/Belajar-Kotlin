8bf75a256306014a79131dc06b87dc7da68b54bf
aldhi saputra <aldhisaputra795@gmail.com>
2024-03-31 23:01:39 +0700

    Tipe data Number

---
+++ src/main/kotlin/Number.kt
@@ -0,0 +1,18 @@
+/**
+Number adalah sebuah tipe data yang khusus digunakan
+untuk menyimpan nilai dalam bentuk numerik
+ **/
+fun main() {
+    val numberOne = 1
+    val numberTwo = 2
+
+    println(numberOne)
+    println(numberOne + numberTwo)
+
+    val maxInt = Int.MAX_VALUE
+    val overRangeInt = Int.MAX_VALUE + 1
+
+    println("Max Int: $maxInt")
+    println("Over range Int: $overRangeInt")
+
+}
