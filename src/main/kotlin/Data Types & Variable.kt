f95e687ccf2098f3032f0c6cc9f3b07198e0ef72
aldhi saputra <aldhisaputra795@gmail.com>
2024-04-20 21:35:12 +0700

    Data Types & Variable

---
+++ src/main/kotlin/Data Types & Variable.kt
@@ -0,0 +1,14 @@
+/*Data types atau tipe data adalah sebuah pengklasifikasian data berdasarkan jenis data tersebut.
+Untuk mengembangkan sebuah program, ada beberapa tipe data yang akan kita pelajari.
+Di antaranya adalah Character, String, Array, Numbers, dan Booleans.
+*/
+
+/*var atau val digunakan untuk mengontrol nilai dari sebuah variabel.
+Dengan kata kunci var, kita bisa mengubah nilai yang sudah diinisialisasikan
+*/
+
+fun main() {
+    val firstWord = "Aku "
+    val lastWord = "Dia"
+    print(firstWord + lastWord)
+}
