dcd0a5cc801907d80eada7971146c7222c312b28
aldhi saputra <aldhisaputra795@gmail.com>
2024-04-22 19:44:57 +0700

    char

---
+++ src/main/kotlin/Char.kt
@@ -0,0 +1,18 @@
+/*Characters direpresentasikan menggunakan tipe Char.
+Untuk mendefinisikan sebuah variabel dengan tipe data Char
+kita bisa menggunakan tanda kutip tunggal (' ')
+*/
+
+//jika kita memasukkan lebih dari 1 (satu) karakter, akan terjadi eror
+
+fun main() {
+    var vocal = 'A'
+
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+}
